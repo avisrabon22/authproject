42b423a4242f5595af4c0ac4bcef0fac001c54c8
avisrabon22 <avisrabon22@gmail.com>
2023-11-15 23:58:06 +0530

    update on controller model

--- src/main/java/com/avijit/authproject/Controller/AuthController.java
+++ src/main/java/com/avijit/authproject/Controller/AuthController.java
@@ -1,12 +1,22 @@
 package com.avijit.authproject.Controller;
 
+
+import com.avijit.authproject.DTO.RequestLoginDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
 
-
-    public void login(){
+    @RequestMapping("login")
+    public void login(@RequestBody RequestLoginDto requestLoginDto){
 
     }
 
+    @PostMapping("/valid"  )
     public void validate(){
 
     }
